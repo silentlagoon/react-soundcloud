795bb14ec155d9aa2f6c5fed88ee63abcee56167
Dmitry Kurilenko <silentlagoon@gmail.com>
2017-08-28 18:40:30 +0300

    remove logic from index.js file and move it to component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
 
 const tracks = [
     {
@@ -11,13 +12,7 @@ const tracks = [
 ];
 
 ReactDOM.render(
-    <div>
-        {
-            tracks.map((track, key) => {
-                return <div className="track" key={key}>{track.title}</div>
-            })
-        }
-    </div>,
+    <Stream tracks={tracks} />,
     document.getElementById('app')
 );
 
